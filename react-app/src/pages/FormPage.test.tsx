9f89ff317725759213939b6db70336120ff8aac9
VladimirG91 <vladimir.glazkov91@gmail.com>
2023-03-30 00:16:30 +0600

    fix: remove unusev fireEvent in test

--- react-app/src/pages/FormPage.test.tsx
+++ react-app/src/pages/FormPage.test.tsx
@@ -1,5 +1,5 @@
 import { FormPage } from './FormPage';
-import { fireEvent, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { Card } from 'components/Card';
 
 describe('FormPage', () => {
