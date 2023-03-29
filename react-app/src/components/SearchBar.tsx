1371ffe4afd5ab6959c7c7252fda155f52702a46
VladimirG91 <vladimir.glazkov91@gmail.com>
2023-03-29 22:04:00 +0600

    feat: change searchbar on hooks

--- react-app/src/components/SearchBar.tsx
+++ react-app/src/components/SearchBar.tsx
@@ -1,55 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-interface ISearchBarProps {
-  placeholder?: string;
-}
-interface ISearchBarState {
-  value: string;
-}
-
-class SearchBar extends React.Component<ISearchBarProps, ISearchBarState> {
-  constructor(props: ISearchBarProps) {
-    super(props);
-    this.state = { value: localStorage.getItem('searchValue') || '' };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  componentDidMount(): void {
+function SearchBar() {
+  const [value, setValue] = useState<string>(() => {
     const searchValue = localStorage.getItem('searchValue');
-    if (searchValue) {
-      this.setState({ value: searchValue });
-    }
-  }
+    return searchValue ? searchValue : '';
+  });
 
-  componentWillUnmount(): void {
-    localStorage.setItem('searchValue', this.state.value);
-  }
+  useEffect(() => {
+    localStorage.setItem('searchValue', value);
+  }, [value]);
 
-  handleChange(event: React.ChangeEvent<HTMLInputElement>) {
-    this.setState({ value: event.target.value });
-  }
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+  };
 
-  handleSubmit(event: React.FormEvent<HTMLFormElement>) {
-    localStorage.setItem('searchValue', this.state.value);
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    localStorage.setItem('searchValue', value);
     event.preventDefault();
-  }
+  };
 
-  render() {
-    return (
-      <form className="form-search" onSubmit={this.handleSubmit}>
-        <label>
-          <input
-            className="form-input"
-            type="text"
-            value={this.state.value}
-            onChange={this.handleChange}
-          />
-        </label>
-        <input className="form-btn" type="submit" value="Search" />
-      </form>
-    );
-  }
+  return (
+    <form className="form-search" onSubmit={handleSubmit}>
+      <label>
+        <input className="form-input" type="text" value={value} onChange={handleChange} />
+      </label>
+      <input className="form-btn" type="submit" value="Search" />
+    </form>
+  );
 }
 
 export { SearchBar };
