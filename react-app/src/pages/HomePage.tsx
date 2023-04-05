1456a1ef6e4421bbf313ccec329b7acc340991be
VladimirG91 <vladimir.glazkov91@gmail.com>
2023-04-05 17:53:38 +0600

    feat: add timeout api request

--- react-app/src/pages/HomePage.tsx
+++ react-app/src/pages/HomePage.tsx
@@ -4,16 +4,29 @@ import { SearchBar } from '../components/SearchBar';
 import { Card } from '../components/Card';
 import { ICardProps } from '../components/Card';
 
+const TIMEOUT = 2000;
 function HomePage() {
   const [dataCards, setDataCards] = useState<ICardProps[]>([]);
+  const [loading, setLoading] = useState(false);
+  async function getData() {
+    setLoading(true);
+    setTimeout(async () => {
+      try {
+        await fetch('https://642c494a208dfe25472ca61d.mockapi.io/10_movies')
+          .then((res) => {
+            return res.json();
+          })
+          .then((arr) => {
+            setDataCards(arr);
+          });
+        setLoading(false);
+      } catch (err) {
+        console.log(err);
+      }
+    }, TIMEOUT);
+  }
   useEffect(() => {
-    fetch('https://642c494a208dfe25472ca61d.mockapi.io/10_movies')
-      .then((res) => {
-        return res.json();
-      })
-      .then((arr) => {
-        setDataCards(arr);
-      });
+    getData();
   }, []);
 
   const cards = dataCards.map((obj) => (
@@ -30,7 +43,9 @@ function HomePage() {
   return (
     <div className="wrapper">
       <SearchBar />
-      <div className="content">{cards}</div>
+      <div className="content">
+        {loading ? <h1 className="textLoading">Идет загрузка...</h1> : cards}
+      </div>
     </div>
   );
 }
