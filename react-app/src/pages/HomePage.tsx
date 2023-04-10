7d4a77766569b764da6d19f38a4e5ff467b8ae18
VladimirG91 <vladimir.glazkov91@gmail.com>
2023-04-10 15:59:27 +0600

    fix: first render

--- react-app/src/pages/HomePage.tsx
+++ react-app/src/pages/HomePage.tsx
@@ -12,6 +12,23 @@ function HomePage() {
   const [noResults, setNoResults] = useState(false);
   const [selectedMovie, setSelectedMovie] = useState<ICardProps | null>(null);
 
+  // useEffect(() => {
+  //   const fetchMovies = async () => {
+  //     try {
+  //       const response = await fetch(`https://642c494a208dfe25472ca61d.mockapi.io/movies`);
+  //       const data = await response.json();
+  //       setMovies(data);
+  //     } catch (err) {
+  //       console.log(err);
+  //     } finally {
+  //       setLoading(false);
+  //     }
+  //   };
+
+  //   setLoading(true);
+  //   fetchMovies();
+  // }, []);
+
   const handleSearch = async (searchTerm: string) => {
     setLoading(true);
     setNoResults(false);
@@ -37,6 +54,21 @@ function HomePage() {
     const searchValue = localStorage.getItem('searchValue');
     if (searchValue) {
       handleSearch(searchValue);
+    } else {
+      const fetchMovies = async () => {
+        try {
+          const response = await fetch(`https://642c494a208dfe25472ca61d.mockapi.io/movies`);
+          const data = await response.json();
+          setMovies(data);
+        } catch (err) {
+          console.log(err);
+        } finally {
+          setLoading(false);
+        }
+      };
+
+      setLoading(true);
+      fetchMovies();
     }
   }, []);
 
